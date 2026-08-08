9feba7088e44ed4f47ca11d79f4874e161dfabb2
Hassan-nawaz834 <hassannawazcui@gmail.com>
2026-08-08 06:10:17 +0500

    fix vercel in client

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,14 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
+    sourcemap: false
   },
   server: {
     proxy: {
